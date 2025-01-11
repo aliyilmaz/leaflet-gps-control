17538b99d0ab933a7bd56232f80a4e0f66bf38b7
Ali Yılmaz <aliyilmaz.work@gmail.com>
2025-01-11 16:57:07 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,109 @@
-# leaflet-gps-control
+# Leaflet GPS Control
+
+This is a custom Leaflet control to track the user's GPS location on a map. It allows users to toggle the location tracking feature on and off, and the map will automatically update and center on the user's location. The marker representing the user's location will animate by growing and shrinking.
+
+## Features
+- Toggle location tracking on/off.
+- GPS location updates.
+- Animated marker (growing and shrinking effect).
+- Map re-centering based on the updated GPS coordinates.
+- Customizable button title.
+
+## Requirements
+- [Leaflet.js](https://leafletjs.com/) (v1.x+)
+
+## Installation
+
+### 1. Include Leaflet.js and Leaflet CSS
+
+Ensure that you include the Leaflet.js library in your HTML. You can use a CDN or download it manually.
+
+```html
+<!-- Leaflet CSS -->
+<link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+
+<!-- Leaflet JS -->
+<script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+```
+
+### 2. Include the GPS Location Control Files
+You should include the custom CSS and JavaScript files in your project. Make sure both ``leaflet-gps-control.css`` and ``leaflet-gps-control.js`` are in the same directory as your HTML file.
+
+```html
+<!-- Include the GPS Location Control CSS -->
+<link rel="stylesheet" href="leaflet-gps-control.css" />
+
+<!-- Include the GPS Location Control JS -->
+<script src="leaflet-gps-control.js"></script>
+```
+
+## Usage
+### 1. Create a Map
+Create a basic Leaflet map where the GPS location control will be added.
+```html
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+// Add OpenStreetMap Tile Layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+```
+
+### 2. Add the GPS Location Control
+Add the custom GPS location control to your map. The control will be positioned at the top right corner by default.
+```html
+L.control.gpsLocation({ 
+    position: 'topright',  // Position of the control button
+    title: 'Start GPS Tracking'  // Title that appears when the user hovers over the button
+}).addTo(map);
+```
+
+### 3. Add a Map Container in HTML
+Ensure that your HTML contains a container for the map.
+```html
+<div id="map"></div>
+```
+## How It Works
+- The **GPS Location Control** will appear as a button on the map.
+- When clicked, the button will toggle location tracking on and off.
+- When tracking is active, the browser's geolocation API will continuously update the user's position.
+- The map will automatically recenter on the user's current location.
+- The marker representing the user's location will animate by growing and shrinking.
+
+## Customization
+### Change the Button Position
+The position of the GPS tracking button can be customized using the ``position`` option when creating the control. It can be set to one of the following values:
+- ``'topleft'``
+- ``'topright'``
+- ``'bottomleft'``
+- ``'bottomright'``
+
+Example:
+```javascript
+L.control.gpsLocation({ position: 'bottomright' }).addTo(map);
+```
+
+### Change the Marker Size or Animation
+You can adjust the size of the animated marker by modifying the ``width`` and ``height`` properties in the CSS, or change the ``@keyframes`` animation to control how the marker grows and shrinks.
+
+```css
+.animated-marker .marker {
+    width: 30px;  /* Adjust size */
+    height: 30px;  /* Adjust size */
+    animation: growShrink 1s ease-in-out infinite;
+}
+```
+### Set a Custom Title for the Button
+You can customize the ``title`` of the GPS tracking button. The ``title`` is the text that appears as a tooltip when the user hovers over the button.
+
+Example:
+```javascript
+L.control.gpsLocation({
+    position: 'topright', 
+    title: 'Click to Start Location Tracking'  // Custom title for the button
+}).addTo(map);
+```
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Contributing
+Feel free to fork the repository and contribute by opening issues or creating pull requests.
---
+++ leaflet-gps-control.js
@@ -0,0 +1,115 @@
+L.Control.GpsLocation = L.Control.extend({
+    options: {
+        position: 'topleft',  // The location of the button on the map
+        title: 'Location tracking', // The title of the button
+    },
+
+    initialize: function (options) {
+        L.Util.setOptions(this, options);
+    },
+
+    onAdd: function (map) {
+        this._map = map;
+
+        // Create the button container
+        this._container = L.DomUtil.create('div', 'leaflet-bar leaflet-control');
+        this._button = L.DomUtil.create('a', '', this._container);
+        this._button.href = '#';
+        this._button.title = this.options.title;
+
+        // Add the SVG icon to the button
+        this._icon = L.DomUtil.create('div', '', this._button);
+        this._icon.innerHTML = `
+            <svg style="margin: 7px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-crosshair" viewBox="0 0 16 16">
+                <path d="M8.5.5a.5.5 0 0 0-1 0v.518A7 7 0 0 0 1.018 7.5H.5a.5.5 0 0 0 0 1h.518A7 7 0 0 0 7.5 14.982v.518a.5.5 0 0 0 1 0v-.518A7 7 0 0 0 14.982 8.5h.518a.5.5 0 0 0 0-1h-.518A7 7 0 0 0 8.5 1.018zm-6.48 7A6 6 0 0 1 7.5 2.02v.48a.5.5 0 0 0 1 0v-.48a6 6 0 0 1 5.48 5.48h-.48a.5.5 0 0 0 0 1h.48a6 6 0 0 1-5.48 5.48v-.48a.5.5 0 0 0-1 0v.48A6 6 0 0 1 2.02 8.5h.48a.5.5 0 0 0 0-1zM8 10a2 2 0 1 0 0-4 2 2 0 0 0 0 4"/>
+            </svg>
+        `;
+        this._icon.style.width = '24px';
+        this._icon.style.height = '24px';
+
+        // Add event listener to toggle tracking when the button is clicked
+        L.DomEvent.on(this._button, 'click', this._toggleTracking, this);
+
+        return this._container;
+    },
+
+    _toggleTracking: function (e) {
+        e.preventDefault();
+
+        // Toggle the location tracking feature
+        if (this._tracking) {
+            this._stopTracking();
+        } else {
+            this._startTracking();
+        }
+    },
+
+    _startTracking: function () {
+        if (this._tracking) return;
+
+        this._tracking = true;
+        this._button.style.color = 'rgb(22 149 55)';  // Change button color when tracking is active
+
+        // Start location tracking if geolocation is available
+        if (navigator.geolocation) {
+            this._watchId = navigator.geolocation.watchPosition(this._onLocationUpdate.bind(this), this._onLocationError.bind(this), {
+                enableHighAccuracy: true,
+                maximumAge: 1000,
+                timeout: 5000
+            });
+        } else {
+            alert('Geolocation is not supported by this browser.');
+        }
+    },
+
+    _stopTracking: function () {
+        if (!this._tracking) return;
+
+        this._tracking = false;
+        this._button.style.color = '';  // Reset button color when tracking is stopped
+
+        // Stop location tracking
+        if (this._watchId) {
+            navigator.geolocation.clearWatch(this._watchId);
+            this._watchId = null;
+        }
+
+        // Remove the location marker from the map
+        if (this._marker) {
+            this._map.removeLayer(this._marker);
+            this._marker = null;
+        }
+    },
+
+    _onLocationUpdate: function (position) {
+        var lat = position.coords.latitude;
+        var lon = position.coords.longitude;
+
+        // Create or update the location marker with CSS animation
+        if (!this._marker) {
+            // Create a marker using divIcon to apply CSS animation
+            this._marker = L.marker([lat, lon], {
+                icon: L.divIcon({
+                    className: 'animated-marker',  // CSS class for animation
+                    html: '<div class="marker"></div>',  // Div element for the marker
+                    iconSize: [24, 24]  // Set the size of the marker
+                })
+            }).addTo(this._map);
+        } else {
+            this._marker.setLatLng([lat, lon]);
+        }
+
+        // Re-center the map to the updated location
+        this._map.setView([lat, lon], this._map.getZoom());
+    },
+
+    _onLocationError: function (error) {
+        console.error(error.message);
+        alert('Could not get location. Please allow location access or make sure your location services are enabled.');
+    }
+});
+
+// Add the control to the map
+L.control.gpsLocation = function (options) {
+    return new L.Control.GpsLocation(options);
+};
